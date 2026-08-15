b3dff94626e2b56698d9a7d28837fd6bf746bcb5
Julaly <mtm296185@gmail.com>
2026-08-15 15:19:44 +0700

    cam

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -21,6 +21,12 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private float xInput = 0f;
 
+    [SerializeField]
+    private GameObject BallLine;
+
+    [SerializeField]
+    private GameObject cam;
+
     public static GameManager instance;
 
     private void Awake()
@@ -31,6 +37,7 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        CameraBehindCueBall();
         SetBall(Ballcolor.Red, 1);
         SetBall(Ballcolor.Yellow, 2);
         SetBall(Ballcolor.Green, 3);
@@ -52,7 +59,13 @@ public class GameManager : MonoBehaviour
         if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
             xInput = -0.1f;
         else if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+            xInput = 0.1f;
+
+        else 
             xInput = 0f;
+
+        if(Keyboard.current.backspaceKey.wasPressedThisFrame)
+            StopBall();
     }
 
  
@@ -70,11 +83,35 @@ public class GameManager : MonoBehaviour
     {
         Rigidbody rd =cueBall.GetComponent<Rigidbody>();
         rd.AddRelativeForce(Vector3.forward * 50, ForceMode.Impulse);
+
+        BallLine.SetActive(false);
+        cam.transform.parent = null;
+        cam.transform.position = new Vector3(0f , 30f , -42f);
+        cam.transform.eulerAngles = new Vector3(45f, 0f, 0f);
+
     }
 
     private void RotateBall()
     {
         if(cueBall != null)
             cueBall.transform.Rotate(new Vector3(0f , xInput , 0f));
+    }
+
+    private void StopBall()
+    {
+       Rigidbody rb = cueBall.GetComponent<Rigidbody>();
+        rb.linearVelocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        cueBall.transform.eulerAngles = Vector3.zero;
+
+        BallLine.SetActive(true);
+        CameraBehindCueBall();
+
+    }
+    private void CameraBehindCueBall()
+    {
+        cam.transform.parent = cueBall.transform;
+        cam.transform.position = cueBall.transform.position + new Vector3(0f , 7f , -15f);
+        cam.transform.eulerAngles = new Vector3(30f, 0f, 0f);
     }
 }
